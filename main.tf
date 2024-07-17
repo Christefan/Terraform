337fa04a239cbfbabe067925990d9c346cbceb53
Christian Capodanno <70919665+Christefan@users.noreply.github.com>
2024-07-18 01:30:38 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,34 +1,19 @@
 provider "google" {
-  project     = "jenkins-project-423708"
-  #credentials = file("jenkins-project-423708.json")
-  region      = "us-central1-c"
-
+  project = "your-project-id"
+  credentials = file("path/to/your/google_credentials.json")
+  region = "us-central1"
 }
 
 resource "google_compute_network" "vpc1" {
   name                    = "my-custom-network-1"
-  auto_create_subnetworks = true
-
-  # Aggiunta di regole del firewall per controllare il traffico di rete
-  firewall {
-    name    = "allow-http"
-    network = google_compute_network.vpc1.name
-
-    allow {
-      protocol = "tcp"
-      ports    = ["80", "443"]
-    }
-
-    source_ranges = ["0.0.0.0/0"]
-  }
-  project     = "jenkins-project-423708"
+  auto_create_subnetworks = false
 }
 
-resource "google_compute_subnetwork" "my-custom-subnet1" {
+resource "google_compute_subnetwork" "subnet1" {
   name          = "my-custom-subnet-1"
-  ip_cidr_range = "10.255.196.0/24"
+  ip_cidr_range = "10.0.1.0/24"
   network       = google_compute_network.vpc1.name
-  region        = "us-central1-c"
+  region        = "us-central1"
 }
 
 
